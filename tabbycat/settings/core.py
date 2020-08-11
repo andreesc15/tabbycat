a620a03a0fb91da0828812289ddb921bed3919ca
Étienne Beaulé <beauleetienne0@gmail.com>
2020-08-11 04:03:46 -0300

    Add browable API for core config

--- tabbycat/settings/core.py
+++ tabbycat/settings/core.py
@@ -313,12 +313,15 @@ DYNAMIC_PREFERENCES = {
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ],
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
 }
